6f0dcf9a55c0fb0769b2e73149a67acb37c951cc
Kim <mjkim1201@naver.com>
2020-02-16 11:32:44 +0900

    비지니스 로직 2 xml 어노테이션 DI 익히기 :

---
+++ BoardWeb/src/main/java/com/springbook/biz/user/impl/UserDAO.java
@@ -0,0 +1,66 @@
+package com.springbook.biz.user.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.springframework.stereotype.Repository;
+
+import com.springbook.biz.common.JDBCUtil;
+import com.springbook.biz.user.UserVO;
+
+
+@Repository("userDAO")
+public class UserDAO {
+
+	private Connection conn =null;
+	private PreparedStatement stmt = null;
+	private ResultSet rs =null;
+	
+	
+	
+	//SQL
+	
+	private final String USER_GET ="select * from users where id=? and password=?";
+	
+	//회원등록 
+	
+	public UserVO getUser(UserVO vo)
+	{
+		UserVO user=null;
+		
+		
+		try {
+			
+			
+			System.out.println("User 객체 getUser 기능처리");
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(USER_GET);
+			
+			stmt.setString(1, vo.getId());
+			stmt.setString(2, vo.getPassword());
+			rs =stmt.executeQuery();
+			if(rs.next())
+			{
+				user= new UserVO();
+				user.setId(rs.getString("ID"));
+				user.setPassword(rs.getString("PASSWORD"));
+				user.setName(rs.getString("NAME"));
+				user.setRole(rs.getString("ROLE"));
+			}
+			System.out.println(user.getId());
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally
+		{
+			JDBCUtil.close(rs,stmt,conn);
+		}
+		return user;
+	}
+	
+	
+	
+}

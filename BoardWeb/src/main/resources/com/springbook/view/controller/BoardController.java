6b665669b9bc1040c624c1399c94094afeaa5bc0
Kim <mjkim1201@naver.com>
2020-02-20 16:01:43 +0900

    spring mvc 마무리 / jdbctemplete 사용 / 어노테이션 기법 / xml 2-Layered 아키텍쳐

--- BoardWeb/src/main/resources/com/springbook/view/controller/BoardController.java
+++ BoardWeb/src/main/resources/com/springbook/view/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.springbook.view.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
-import com.springbook.biz.board.impl.BoardDAO;
 import com.springbook.biz.user.UserVO;
 import com.springbook.biz.user.impl.UserDAO;
 
@@ -19,12 +20,16 @@ import com.springbook.biz.user.impl.UserDAO;
 @SessionAttributes("board")
 @Controller
 public class BoardController {
+									// Component인 Controller가 있음으로 Controller 객체가 생성됨과 동시에 Autowired 를 통해 주입시킨다.
+	@Autowired						// BoardService를 주입시키고 boardDAO를 직접 사용하지 않는다.
+	private BoardService boardService;
+	
 
 	
 	@RequestMapping("/deleteBoard.do")
-	public String deleteBoard(@ModelAttribute("board") BoardVO vo,BoardDAO boardDAO){// handleRequest(HttpServletRequest request, HttpServletResponse response) 
+	public String deleteBoard(@ModelAttribute("board") BoardVO vo){// handleRequest(HttpServletRequest request, HttpServletResponse response) 
 		
-		boardDAO.deleteBoard(vo);
+		boardService.deleteBoard(vo);
 
 		return "getBoardList.do";
 
@@ -32,38 +37,34 @@ public class BoardController {
 	}
 	
 	@RequestMapping("/getBoard.do")
-	public String getBoard(BoardVO vo , BoardDAO boardDAO, Model model) {//handleRequest(HttpServletRequest request, HttpServletResponse response)
+	public String getBoard(BoardVO vo , Model model) {//handleRequest(HttpServletRequest request, HttpServletResponse response)
 		
-		model.addAttribute("board",boardDAO.getBoard(vo));
+		model.addAttribute("board",boardService.getBoard(vo));
 		return "getBoard.jsp";
 		
 	}
 	
 	@RequestMapping("/getBoardList.do")
 	public String getBoardList(@RequestParam(value = "searchCondition", defaultValue = "TITLE", required = false) String contition
-			,@RequestParam(value = "searchKeyword", defaultValue = "", required = false) String keyword, BoardVO vo,
-			BoardDAO boardDAO, Model model){ // handleRequest(HttpServletRequest request, HttpServletResponse response) 
-		//BoardVO vo = new 	BoardVO();
-	//	BoardDAO boardDAO = new BoardDAO();
-
-	//	List<BoardVO> boardList = boardDAO.getBoardList(vo);
-		
-	//	HttpSession session = request.getSession();
-	//	session.setAttribute("boardList", boardList);
-	//	return "getBoardList";
+			,@RequestParam(value = "searchKeyword", defaultValue = "", required = false) String keyword, BoardVO vo
+			, Model model){ // handleRequest(HttpServletRequest request, HttpServletResponse response) 
 		
 		
-	//	ModelAndView mav = new ModelAndView();
+		if(vo.getSearchCondition() ==null) vo.setSearchCondition("TITLE");
+		if(vo.getSearchKeyword()==null) vo.setSearchKeyword("");
+		// 키워드 없이 전송 누르면 기본적으로 제목에 해당되는 모든 값을 출력 
+		
+		
 		System.out.println(keyword);
 		System.out.println(contition);
-		model.addAttribute("boardList", boardDAO.getBoardList(vo));
+		model.addAttribute("boardList", boardService.getBoardList(vo));
 		
 		return "getBoardList.jsp";
 	}
 	
 	
 	@RequestMapping(value = "/insertBoard.do")	// xml에서 HandlerMapping 안에 property로 넣는 과정 어노테이션 
-	public String insertBoard(BoardVO vo,BoardDAO boardDAO)//insertBoard(HttpServletRequest request)
+	public String insertBoard(BoardVO vo)//insertBoard(HttpServletRequest request)
 	
 	{
 //		String title = request.getParameter("title");
@@ -78,7 +79,7 @@ public class BoardController {
 
 
 		//BoardDAO boardDAO = new BoardDAO();	// 이렇게 처리하면 바로 사용자 입력 값과 vo 객체 그릭 값 설정을 모두 자동으로 처리 
-		boardDAO.insertBoard(vo);
+		boardService.insertBoard(vo);
 
 			//ModelAndView mav = new ModelAndView();
 			//	mav.setViewName("getBoardList.do");
@@ -116,10 +117,10 @@ public class BoardController {
 	
 
 	@RequestMapping("/updateBoard.do")
-	public String UpdateBoard( BoardVO vo,BoardDAO boardDAO) {//handleRequest(HttpServletRequest request, HttpServletResponse response)
+	public String UpdateBoard( BoardVO vo) {//handleRequest(HttpServletRequest request, HttpServletResponse response)
 	
 	
-		boardDAO.updateBoard(vo);
+		boardService.updateBoard(vo);
 		
 		return "getBoardList.do";
 
